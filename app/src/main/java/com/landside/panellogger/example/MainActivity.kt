c9366b130921f833ddd3ba963f7403481382be26
Landscape <494326656@qq.com>
2021-08-05 16:56:23 +0800

    init

---
+++ app/src/main/java/com/landside/panellogger/example/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.landside.panellogger.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.landside.panellogger.R
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
